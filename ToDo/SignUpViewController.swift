0521c969c40bf962658366e117123388024d0cd1
Ilya Matveev <matveev88ilya@yandex.ru>
2019-12-26 15:08:31 +0300

    Add scroll with keyboard

--- ToDo/SignUpViewController.swift
+++ ToDo/SignUpViewController.swift
@@ -15,6 +15,7 @@ class SignUpViewController: UIViewController {
     private var newPassword: String?
     private var newRePassword: String?
 
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var signUpOutlet: UIButton!
     @IBOutlet var toolBarOutlet: UIToolbar!
     @IBOutlet var loginOutlet: UITextField!
@@ -55,6 +56,9 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
         navigationController?.setNavigationBarHidden(false, animated: false)
 
         signUpOutlet.layer.cornerRadius = 6
@@ -73,6 +77,20 @@ class SignUpViewController: UIViewController {
 
     @objc func doneAction() {
         view.endEditing(true)
+    }
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
 }
