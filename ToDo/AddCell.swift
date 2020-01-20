a5ed92b33b409fd13b6ce14333ad99fa0a60ad6f
Ilya Matveev <matveev88ilya@yandex.ru>
2020-01-20 16:33:21 +0300

    Add AddCell

---
+++ ToDo/AddCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddCell.swift
+//  ToDo
+//
+//  Created by Ilya Matveev on 19.01.2020.
+//  Copyright © 2020 Ilya Matveev. All rights reserved.
+//
+
+import UIKit
+
+class AddCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
